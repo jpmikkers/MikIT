8383a50582afa2b6a40a671f5673b3aa825a3519
jpmikkers <jean.paul.mikkers@gmail.com>
2025-04-14 13:13:44 +0200

    remove duplicated license text

--- mmod_md2.cpp
+++ mmod_md2.cpp
@@ -1,22 +1,5 @@
 /*
 
-(c) Copyright 1996, 1997 Jean-Paul Mikkers 
-
-This file is part of MikIT.
-
-MikIT is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as 
-published by the Free Software Foundation, either version 3 of 
-the License, or (at your option) any later version.
-
-MikIT is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public 
-License along with MikIT. If not, see <http://www.gnu.org/licenses/>.
-
 File:           MMOD_MD2.CPP
 Description:    -
 Version:        1.00 - original
